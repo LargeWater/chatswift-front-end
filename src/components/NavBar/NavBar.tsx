5d0a382ea39ab067fdc33b22059c7b8bd27f61e3
Nicholas Bailey <nickcyg1992@gmail.com>
2023-10-22 00:48:20 -0500

    begin work on redoing navbar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,23 +1,74 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import styled from '@emotion/styled'
+
+const Theme = {
+  colors: {
+    bg: `#fff`,
+    dark: `#24292e`,
+    light: `#EEEEEE`,
+  },
+  fonts: {
+    heading: `Inter, sans-serif`,
+  }
+}
+
+const Nav = styled.nav`
+  background: ${Theme.colors.dark};
+  font-family: ${Theme.fonts.heading};
+  color: ${Theme.colors.light};
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  a { color: white; text-decoration: none; }`;
+
+  const Ul = styled.ul({
+    display: 'flex',
+    flexWrap: 'nowrap',
+    overflowX: 'auto',
+  })
+
+const Li = styled.li`
+  flex: 0 0 auto;
+  -webkit-box-align: center;
+  -webkit-box-pack: center;
+  -webkit-tap-highlight-color: transparent;
+  align-items: center;
+  color: #999;
+  height: 100%;
+  justify-content: center;
+  text-decoration: none;
+  -webkit-box-align: center;
+  -webkit-box-pack: center;
+  -webkit-tap-highlight-color: transparent;
+  align-items: center;
+  color: #999;
+  display: flex;
+  font-size: 14px;
+  height: 50px;
+  justify-content: center;
+  line-height: 16px;
+  margin: 0 10px ;
+  text-decoration: none`;
+
 
 const NavBar = ({ user, handleLogout }) => {
   return (
-    <nav>
+    <Nav>
       {user ?
-        <ul>
-          <li>Welcome, {user.name}</li>
-          <li><NavLink to="" onClick={handleLogout}>LOG OUT</NavLink></li>
-          <li><NavLink to="/auth/change-password">Change Password</NavLink></li>
-          <li><NavLink to="/chats">Chats</NavLink></li>
-        </ul>
+        <Ul>
+          <Li>Welcome, {user.name}</Li>
+          <Li><NavLink to="" onClick={handleLogout}>LOG OUT</NavLink></Li>
+          <Li><NavLink to="/auth/change-password">Change Password</NavLink></Li>
+          <Li><NavLink to="/chats">Chats</NavLink></Li>
+        </Ul>
       :
-        <ul>
-          <li><NavLink to="/auth/login">Log In</NavLink></li>
-          <li><NavLink to="/auth/signup">Sign Up</NavLink></li>
-        </ul>
+        <Ul>
+          <Li><NavLink to="/auth/login">Log In</NavLink></Li>
+          <Li><NavLink to="/auth/signup">Sign Up</NavLink></Li>
+        </Ul>
       }
-    </nav>
+    </Nav>
   )
 }
 
